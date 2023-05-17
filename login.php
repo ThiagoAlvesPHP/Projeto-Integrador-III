bdc4458d7a487ff6af2157f9a8e0cfc0db2ca075
ThiagoAlvesPHP <thiagoalves@ltdeveloper.com.br>
2023-05-17 08:32:50 -0300

    criação de repositorio e pagina de login

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+
+    <link rel="stylesheet" href="./assets/css/pages/login.css">
+</head>
+
+<body>
+    <section class="container">
+        <div class="content">
+            <h1>Aplicativo Fitness Academia</h1>
+            <hr>
+            <h3>Acesse aqui</h3>
+            <form action="" method="POST">
+                <label for="usuario">Usuário</label>
+                <input type="text" name="usuario" class="input" id="usuario" required>
+                <label for="senha">Senha</label>
+                <input type="password" name="senha" class="input" id="senha" required>
+
+                <button>Next</button>
+            </form>
+            <hr>
+            <a href="" class="cadastrar">Cadastre-se</a>
+        </div>
+    </section>
+</body>
+
+</html>
